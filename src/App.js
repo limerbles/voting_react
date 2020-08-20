32b6d102cfd5ac716ae52dacf432600eeb60dcab
zli289 <zli289@gwu.edu>
2020-08-19 18:00:06 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state= {
+      languages: [
+        {name: "Python", votes: 0},
+        {name: "Go", votes: 0},
+        {name: "Javascript", votes: 0},
+        {name: "C++", votes: 0},
+      ]
+    };
+  }
+
+  vote(i) {
+    let after= [...this.state.languages];
+    after[i].votes++;
+    this.setState({languages: after});
+  }
+
+  render() {
+    return (
+      <>
+      <h1>Vote your language</h1>
+      <div class="languages">
+        {
+          this.state.languages.map((lang, i)=>
+            <div key={i} className="language">
+              <div className="VoteCount">
+                {lang.votes}
+              </div>
+              <div className="languageName">
+                {lang.name}
+              </div>
+              <button onClick={this.vote.bind(this,i)}>Click Here</button>
+            </div>
+          )
+        }
+      </div>
+      </>
+    )
+  }
 }
 
 export default App;
